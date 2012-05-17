74e60fd843cd4a0d6726fa6112fb9e6da6b76b17
ADigitalNative <adigitalnative@gmail.com>
2012-05-17 17:04:08 -0400

    Beginning to switch to Hashie Mashes

--- lib/growler.rb
+++ lib/growler.rb
@@ -2,10 +2,29 @@ class Growler
   require 'faraday'
   require 'json'
   require 'ostruct'
+  require 'hashie'
 
   def connect
     Faraday.new :url => "http://api.hungrlr.dev"
   end
+
+  # Ostruct version
+  # def get_user_growls(display_name, token)
+  #   user_growls = connect.get "v1/feeds/#{display_name}.json?token=#{token}"
+  #   user_growls = user_growls.body
+  #   parsed_growls = JSON.parse(user_growls)
+    
+  #   full_response = OpenStruct.new parsed_growls
+  #   response = OpenStruct.new full_response.items
+  #   response = response.most_recent
+
+  #   growls = []
+  #   response.each do |growl|
+  #     growl = OpenStruct.new growl
+  #     growls << growl
+  #   end
+  #   return growls
+  # end
 
   def get_user_growls(display_name, token)
     user_growls = connect.get "v1/feeds/#{display_name}.json?token=#{token}"
@@ -18,7 +37,8 @@ class Growler
 
     growls = []
     response.each do |growl|
-      growl = OpenStruct.new growl
+      growl = Hashie::Mash.new growl
+      # growl = OpenStruct.new growl
       growls << growl
     end
     return growls
