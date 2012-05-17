38e964eb533cce5a44f6057327e45b9185165e33
ADigitalNative <adigitalnative@gmail.com>
2012-05-17 17:10:51 -0400

    Switched to using Hashie::Mashes instead of OStruct

--- spec/growler_spec.rb
+++ spec/growler_spec.rb
@@ -1,13 +1,24 @@
 require 'spec_helper'
 
 describe Growler do
+  let(:user) { "jq" }
+  let(:token) { "tsukahara" }
 
-  it "provides an array of ostructs" do
+  it "provides an array of hashie mashes" do
     client = Growler.new
-    growls = client.get_user_growls("jq")
+    growls = client.get_user_growls(user, token)
     growls.each do |growl|
-      growl.class.should == OpenStruct
+      growl.class.should == Hashie::Mash
     end
   end
+
+  it "gives all the current user's growls" do
+    pending "TODO: Research accessing DB from gem"
+  end
+
+  it "doesn't provide other growls" do
+    pending "TODO: Research accessing DB from gem"
+  end
+
 
 end
