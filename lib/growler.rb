89b893a556e9722779e7662f58517a58a95c19b9
ADigitalNative <adigitalnative@gmail.com>
2012-05-17 17:00:05 -0400

    Working again

--- lib/growler.rb
+++ lib/growler.rb
@@ -7,12 +7,14 @@ class Growler
     Faraday.new :url => "http://api.hungrlr.dev"
   end
 
-  def get_user_growls(display_name)
-    user_growls = connect.get "v1/feeds/#{display_name}.json"
-    parsed_growls = JSON.parse(user_growls.body)
+  def get_user_growls(display_name, token)
+    user_growls = connect.get "v1/feeds/#{display_name}.json?token=#{token}"
+    user_growls = user_growls.body
+    parsed_growls = JSON.parse(user_growls)
     
     full_response = OpenStruct.new parsed_growls
-    response = OpenStruct.new full_response.items.most_recent
+    response = OpenStruct.new full_response.items
+    response = response.most_recent
 
     growls = []
     response.each do |growl|
@@ -21,7 +23,5 @@ class Growler
     end
     return growls
   end
-
-
 
 end
