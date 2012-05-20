784c06783dbaeae627d04a38e9c3dff22244ecb5
ADigitalNative <adigitalnative@gmail.com>
2012-05-20 15:19:16 -0400

    Added nice return comment for post_mesage

--- lib/growler.rb
+++ lib/growler.rb
@@ -17,20 +17,36 @@ class Growler
     growls = full_response.items.most_recent
   end
 
-  # Not currently functional b/c api doesn't have functionality
-  def post_growl(user, token, type, comment)
-    # @growl_body = "{ 'type':#{type},'comment':#{comment} }"
-    # @growl_body.to_json
+  def post_message(user, token, comment)
+    growl_body = { type: "Message", comment: comment }
     
-    connect.post 'v1/feeds/jq/items', { token: "tsukahara", body: {type: "Message", comment: "Foo" }.to_json }
+    posted = connect.post 'v1/feeds/jq/items', { token: token, body: growl_body.to_json }
+
+    status = posted.status
+    if status == 201
+      puts "Congratulations, you have growled successfully. Here's a summary."
+      puts "Status: #{status}"
+      puts "User: #{user}"
+      puts "Type: Message"
+      puts "Comment: #{comment}"
+    else
+      puts "There was a problem. Please try your growl again."
+      puts "Status: #{status}"
+    end
+    
+    # Eventually may need to use this format to use auth-headers
     # connect.post do |request|
-    #   request.url "v1/feeds/#{user}/items",
-    #     { :token => "#{token}"}
-    #   # request.headers['AUTH-TOKEN'] = token
-    #   request.body @growl_body
+    #   request.url "v1/feeds/#{user}/items", { :token => "#{token}"}
+    # #   # request.headers['AUTH-TOKEN'] = token
+    #   request.body growl_body.to_json
     # end
 
-    # puts "Foo"
+  end
+
+  def post_image(user, token, url, comment)
+    growl_body = { type: "Image", link: url, comment: comment }
+
+    connect.post
   end
 
 end
